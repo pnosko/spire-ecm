a3c0213b09f673db7bbcc0fb5a489adcd974d1e9
Unknown <peter.nosko@gmail.com>
2019-06-15 17:56:59 +0200

    Optimize factors

--- src/main/scala/io/strangeattractor/ecm/ECM.scala
+++ src/main/scala/io/strangeattractor/ecm/ECM.scala
@@ -6,8 +6,31 @@ import io.strangeattractor.ecm.Utils._
 import spire.math._
 
 object ECM {
+  import SafeLong._
 
-  case class FactorizationResult(factor: Num, rest: Num)
+  case class FactorizationResult(factors: Factors, rest: Num)
+
+  object FactorizationResult {
+    def wrap(n: Num): FactorizationResult = FactorizationResult(Factors(n), one)
+    def fromSingleFactor(n: Num, factor: Num): FactorizationResult = {
+      val fs = factors(n, factor)
+      val rest = n / fs.folded
+      FactorizationResult(fs, rest)
+    }
+  }
+
+  def factors(n: Num, g: Num): Factors = {
+    val exp = timesDivisible(n, g)
+    Factors(Map(g -> exp))
+  }
+
+  case class Factors(factors: Map[Num, Int]) {
+    def folded: Num = factors.foldLeft(one){ case (acc, (b, e)) => acc * b ^ e}
+  }
+
+  object Factors {
+    def apply(f: Num): Factors = Factors(Map(f -> 1))
+  }
 
   def factorECM(n: Num, curve: MontgomeryCurve, point: MontgomeryPoint): FactorizationResult = {
     val b1 = 100000
@@ -15,9 +38,19 @@ object ECM {
     factorizeMontgomery(n , b1, curve, point)
   }
 
+  def timesDivisible(n: Num, g: Num): Int = {
+    import SafeLong._
+    var exp = 0
+    var acc = g
+    while (n % acc === zero) {
+      acc *= g
+      exp += 1
+    }
+    exp
+  }
+
   private def factorizeMontgomery(n: Num, b1: Int, curve: MontgomeryCurve, point: MontgomeryPoint): FactorizationResult = {
     val arithmetic = Montgomery.arithmetic(curve, point)
-
 
     def findPower(base: Int): SafeLong = {
       val div = b1 / base
@@ -36,15 +69,18 @@ object ECM {
     val g = n.gcd(multiple.z)
     if (g.isOne) {
       // big step
-      FactorizationResult(n, SafeLong.one)
-    } else
-      FactorizationResult(g, n / g)
+      FactorizationResult.wrap(n)
+    } else {
+      FactorizationResult.fromSingleFactor(n, g)
+    }
   }
 
   def factor(n: Num): FactorizationResult = {
-
     Montgomery.generate(n).fold(
-      foundFactor => FactorizationResult(foundFactor.n, n / foundFactor.n),
+      foundFactor => {
+        val fs = factors(n, foundFactor.n)
+        FactorizationResult(fs, n / fs.folded)
+      },
       {case (c, p) => factorECM(n, c, p)}
     )
   }
