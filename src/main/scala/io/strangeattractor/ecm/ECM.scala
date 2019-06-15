a06bd16f91430208fe66a675cf288023f8eaf9ab
Unknown <peter.nosko@gmail.com>
2019-06-15 21:41:23 +0200

    Fixed tests and such

--- src/main/scala/io/strangeattractor/ecm/ECM.scala
+++ src/main/scala/io/strangeattractor/ecm/ECM.scala
@@ -4,31 +4,11 @@ import io.strangeattractor.ecm.Alias.Num
 import io.strangeattractor.ecm.Montgomery._
 import io.strangeattractor.ecm.Utils._
 import spire.math._
-import spire.std.bigDecimal._
-import spire.syntax.nroot._
-import spire.syntax.trig._
+
 
 object ECM {
   def factorECM(n: Num, curve: MontgomeryCurve, point: MontgomeryPoint): FactorizationResult = {
-    val expectedFactorLength = n.sqrt().toBigDecimal.log(10).toInt
-    factorizeMontgomery(n , getB1(expectedFactorLength), curve, point)
-  }
-
-  private def getB1(expectedFactorLength: Int): Long = expectedFactorLength match {
-    case x if x < 70 => 2900000000L
-    case x if x < 65 => 850000000L
-    case x if x < 60 => 260000000L
-    case x if x < 55 => 110000000L
-    case x if x < 50 => 43000000L
-    case x if x < 45 => 11000000L
-    case x if x < 40 => 3000000L
-    case x if x < 35 => 1000000L
-    case x if x < 30 => 250000
-    case x if x < 25 => 50000
-    case x if x < 20 => 11000
-    case x if x < 15 => 2000
-    case x if x < 12 => 400
-    case _ => 100L
+    factorizeMontgomery(n, b1Bound(n), curve, point)
   }
 
   private def factorizeMontgomery(n: Num, b1: Long, curve: MontgomeryCurve, point: MontgomeryPoint): FactorizationResult = {
--- src/main/scala/io/strangeattractor/ecm/Utils.scala
+++ src/main/scala/io/strangeattractor/ecm/Utils.scala
@@ -1,8 +1,11 @@
 package io.strangeattractor.ecm
 
+import io.strangeattractor.ecm.Alias.Num
 import scodec.bits._
 import spire.math.SafeLong
-
+import spire.std.bigDecimal._
+import spire.syntax.nroot._
+import spire.syntax.trig._
 import scala.annotation.tailrec
 import scala.collection.SeqView
 
@@ -34,5 +37,25 @@ object Utils {
     def loop(n: Int, stream: Stream[Int]): Stream[Int] = next(n, stream)
 
     2 #:: loop(3, primes)
+  }
+
+  private def expectedFactorLength(n: Num): Int = n.sqrt().toBigDecimal.log(10).toInt
+  def b1Bound(n: Num): Long = getB1(expectedFactorLength(n))
+
+  private def getB1(expectedFactorLength: Int): Long = expectedFactorLength match {
+    case x if x < 70 => 2900000000L
+    case x if x < 65 => 850000000L
+    case x if x < 60 => 260000000L
+    case x if x < 55 => 110000000L
+    case x if x < 50 => 43000000L
+    case x if x < 45 => 11000000L
+    case x if x < 40 => 3000000L
+    case x if x < 35 => 1000000L
+    case x if x < 30 => 250000
+    case x if x < 25 => 50000
+    case x if x < 20 => 11000
+    case x if x < 15 => 2000
+    case x if x < 12 => 400
+    case _ => 100L
   }
 }
